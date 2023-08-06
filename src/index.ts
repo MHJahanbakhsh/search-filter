773a0fd0194f508dbb4bf4a0919517c1501c3214
MohammadHossein Jahanbakhsh <mammadjahanbakhsh@yahoo.com>
2023-08-06 22:36:13 +0330

    finish js logic

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,20 @@ const list = document.getElementById("list") as HTMLElement;
 const searchInput = document.getElementById("searchbox") as HTMLInputElement;
 const labels = document.getElementsByClassName("item-label");
 const cards = document.getElementsByClassName("card");
-const checkBoxes = document.getElementsByClassName("checkbox");
+const checkBoxes = document.getElementsByClassName(
+  "check-box"
+) as HTMLCollection;
 
 //--------------------states-----------------------
 let items: Iitems;
-const selectedItems: string[] = [];
+let selectedItems: string[] = [];
+document.addEventListener("DOMContentLoaded", () => {
+  const storedSelectedItems = localStorage.getItem("selectedItems");
+  if (storedSelectedItems) {
+    selectedItems = JSON.parse(storedSelectedItems!);
+  }
+  console.log({ selectedItems });
+});
 
 //--------------------Functions-------------------
 function searchCards() {
@@ -34,18 +43,41 @@ const debounce = (fn: Function, ms = 300) => {
   };
 };
 
+function toggleSelection(title: string) {
+  if (selectedItems.includes(title)) {
+    selectedItems = selectedItems.filter((each) => each !== title);
+  } else {
+    selectedItems.push(title);
+  }
+
+  localStorage.setItem("selectedItems", JSON.stringify(selectedItems));
+}
+
 const createNewItemElement = function (taskString: string) {
   const listItem = document.createElement("li");
   listItem.classList.add("card");
 
   const checkBox = document.createElement("input");
-  checkBox.className = "checkbox";
-
-  const label = document.createElement("label");
-  label.className = "item-label";
-
+  checkBox.classList.add("check-box");
   checkBox.type = "checkBox";
 
+  if (selectedItems && selectedItems.includes(taskString)) {
+    checkBox.checked = true;
+  }
+  checkBox.addEventListener("click", (e) => {
+    toggleSelection(listItem.childNodes[1].textContent!);
+    console.log(selectedItems);
+    selectedItems.forEach((each, index) => {
+      items.data[index] = each;
+    });
+    console.log(checkBox.checked);
+    if (checkBox.checked) {
+      checkBox.parentElement?.parentElement?.prepend(checkBox.parentElement);
+    }
+  });
+
+  const label = document.createElement("label");
+  label.classList.add("item-label");
   label.innerText = taskString;
 
   // Each element needs appending
@@ -66,20 +98,25 @@ searchInput.addEventListener(
   "input",
   debounce(() => searchCards())
 );
-console.log(checkBoxes);
 for (let i = 0; i < checkBoxes.length; i++) {
-  checkBoxes[i].addEventListener("change", function () {
-    console.log("changed");
-  });
+  checkBoxes[i].addEventListener("", function () {});
 }
 
 getItems().then((result) => {
   items = result;
-  console.log("runnign");
 
+  items.data.forEach((item) => {
+    selectedItems.forEach((selectedItem) => {
+      if (item === selectedItem) {
+        //change the first element of the refrence array
+        items.data.unshift(items.data.splice(items.data.indexOf(item), 1)[0]);
+      }
+    });
+  });
   items.data.forEach((each) => {
     list?.appendChild(createNewItemElement(each));
   });
 });
 
-//------------- search function--------------
+
+
